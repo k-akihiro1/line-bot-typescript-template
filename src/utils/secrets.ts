8966ab695986dc1af949bd90e1c76bd4704a882e
k_akihiro1 <k.shuhari@gmail.com>
2023-02-18 14:52:10 +0900

    ポイント処理関数の作成

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -7,3 +7,4 @@ export const LINE_SECRET = process.env.LINE_SECRET as string
 export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY as string
 export const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL as string
 export const GOOGLE_PROJECT_ID = process.env.GOOGLE_PROJECT_ID as string
+export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string
